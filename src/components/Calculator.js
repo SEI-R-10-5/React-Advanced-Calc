0f12f75824b9350b70c5806fe4ba3e4af2d78c7d
Nicola Zanghi <nico.zang7@gmail.com>
2020-11-20 09:04:05 -0500

    Nico Zanghi completes homework, nonfunctional

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,14 +1,75 @@
 import React, { useState } from 'react'
 
 const Calculator = props => {
+    const [initialNumber, setInitialNumber] = useState('')
+    const [operatingNumber, setOperatingNumber] = useState("0")
+    const [operator, PerformOperation] = useState(null)
+    const toggleNegative = useState(false)
+    const isOperating = useState(false)
 
 
-// handleNumberDisplay = number => {
+// setInitialNumber(parseInt([...initialNumber,initialNumber]))
+
+//take this and put it into a function and then concatenate
+//and then parseInt the total concatenation
+
+
+const changeSign = () => {
+    toggleNegative ? 
+    toggleNegative=false 
+    : toggleNegative=true
+    switch (number){
+        case number= initialNumber && toggleNegative===true:
+            initialNumber *-1
+        case number = operatingNumber &&toggleNegative===true:
+            operatingNumber * -1
+    }
+}
+performOperation(initialNumber, operator, operatingNumber)
+    switch(operator) {
+        case"+": ((initialNumber, operatingNumber) => (initialNumber + operatingNumber))
+    break;
+        case"-":  ((initialNumber, operatingNumber) => (initialNumber - operatingNumber))
+    break;
+        case"*": ((initialNumber, operatingNumber) => (initialNumber * operatingNumber))
+    break;
+        case"/": ((initialNumber, operatingNumber) => (initialNumber / operatingNumber))
+    break;
+        case"%": ((initialNumber, operatingNumber) => (initialNumber % operatingNumber))
+    default:
+        console.log('operating switch didn\'t run, what did you even do')
+    }
+}
+
+const numberPrinter = (event, props) => {
+    (isOperating ? 
+    setInitialNumber(event.target.value)
+    :
+    setOperatingNumber(event.target.value))
+    let firstNumber= (new String(initialNumber))
+    let secondNumber=(new String(operatingNumber))
+    parseInt(firstNumber)
+    parseInt(secondNumber)
+    performOperation(
+        firstNumber,
+        secondNumber)
+
+
     
-// }
+}
 
+const clearIt = () => {
+    initialNumber = ''
+    operatingNumber = ''
+    operator = ''
+}
+//need to do string concatenation
+//and tehn parseint the string concatenation
+const calculate = () => {
 
     return (
+
+        //I kind of didn't know what to do at first so I just went through and broke up each button
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
@@ -18,27 +79,31 @@ const Calculator = props => {
                     <button 
                     //      AC button
                         className="calc-button calc-button-top"
-                        onClick={
-                            console.log(`AC button clicked`)
-                        }
+                        onClick={clearIt}
                     >AC
                     </button>
                     <button 
                     //      +/- button
                         className="calc-button calc-button-top"
-                        // onClick={}
+                        onClick={changeSign()}
                     >+/-
                     </button>
                     <button 
                     //      % button
                         className="calc-button calc-button-top"
-                        // onClick={}
+                        onClick={
+                            operate("%"),
+                            isOperating=true                       
+                        }
                     >%
                     </button>
                     <button 
                     //      / button
                     className="calc-button calc-button-op"
-                    // onClick={}
+                    onClick={
+                        operate("/"),
+                        isOperating=true                       
+                    }
                     >/
                     </button>
                 </div>
@@ -46,86 +111,83 @@ const Calculator = props => {
                     <button 
                     //      7 button
                     className="calc-button"
-                    // onClick={}
+                    value='7'
+                    onClick={numberPrinter}
                     >7</button>
                     <button 
                     //      8 button
                     className="calc-button"
-                    // onClick={}
+                    value='8'
+                    onClick={numberPrinter}
                     >8</button>
                     <button 
                     //      9 button
                     className="calc-button"
-                    // onClick={}
+                    value='9'
+                    onClick={numberPrinter}
                     >9</button>
                     <button 
                     //      x button
                     className="calc-button calc-button-op"
                     onClick={
-                        console.log(`button clicked`)
+                        operate("*"),
+                        isOperating=true                        
                     }
                     >x</button>
                 </div>
                 <div className="calc-row">
                     <button 
                         className="calc-button"
-                        onClick={
-                            console.log(`button clicked`)
-                        }    
+                        value = "4"
+                        onClick={numberPrinter}    
                     >4</button>
                     <button 
                     className="calc-button"
-                    onClick={
-                        console.log(`button clicked`)
-                    }
+                    value="5"
+                    onClick={numberPrinter}
                     >5</button>
                     <button 
                         className="calc-button"
                         value="6"
-                        onClick={
-                            console.log(`${value} button clicked`)
-                            
-                        }
+                        onClick={numberPrinter}
                     >6</button>
                     <button 
                         className="calc-button calc-button-op"
                         onClick={
-                            console.log(`button clicked`)
+                            operate("-"),
+                            isOperating=true                           
                         }
                     >-</button>
                 </div>
                 <div className="calc-row">
                     <button 
                         className="calc-button"
-                        onClick={
-                            console.log(`button clicked`)
-                        }    
+                        value="1"
+                        onClick={numberPrinter}
                     >1</button>
                     <button 
                         className="calc-button"
-                        onClick={
-                            console.log(`button clicked`)
-                        }    
+                        value="2"
+                        onClick={numberPrinter}
                     >2</button>
                     <button 
                         className="calc-button"
-                        onClick={
-                            console.log(`button clicked`)
-                        }    
+                        value="3"
+                        onClick={numberPrinter}
                     >3</button>
                     <button 
                         className="calc-button calc-button-op"
                         onClick={
-                            console.log(`button clicked`)
-                        }    
+                            operate("+"),
+                            isOperating=true 
+                        }
                     >+</button>
                 </div>
                 <div className="calc-row">
                     <button 
                         className="calc-button width-2"
-                        onClick={
-                            console.log(`button clicked`)
-                        }    
+                        value="0"
+                        onClick={numberPrinter}
                     >0</button>
                     <button 
                         className="calc-button"
@@ -136,7 +198,7 @@ const Calculator = props => {
                     <button 
                         className="calc-button calc-button-op"
                         onClick={
-                            console.log(`button clicked`)
+                            performOperation()
                         }    
                     >=</button>
                 </div>
