cfe080f6889875ad340fde86aa7f247614a979fb
Nico Zanghi <nico.zang7@gmail.com>
2020-12-20 21:46:44 -0500

    updates error funtionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,214 +1,216 @@
-import React, { useState, /* useEffect */ } from 'react'
+import React, { useState, useEffect} from 'react'
 
 const Calculator = () => {
-    const [initialNumber, setInitialNumber] = useState('')
-    const [operatingNumber, setOperatingNumber] = useState("0")
-    const [operator, PerformOperation] = useState(null)
+    const [operator, setOperator] = useState(null)
     const [error, setError] = useState('')
-    const [toggleNegative, setToggleNegative] = useState(false)
-    const [isOperating, setIsOperating] = useState(false)
+    const [numbers, setNumbers] = useState({'0':'','1':''})
+    const [total, setTotal] = useState('')
 
+    useEffect(()=> {
+        }, [setTotal, setError])
 
-// setInitialNumber(parseInt([...initialNumber,initialNumber]))
-
-//take this and put it into a function and then concatenate
-//and then parseInt the total concatenation
-
-// useEffect(()=>{
-//     setInitialNumber()
-//     setOperatingNumber()
-//     PerformOperation()
-//     setError()
-const numberPrinter = (event) => {
-    (isOperating ? 
-    setInitialNumber(event.target.value)
-    :
-    setOperatingNumber(event.target.value))
-    let firstNumber= (new String(initialNumber))
-    let secondNumber=(new String(operatingNumber))
-    parseInt(firstNumber)
-    parseInt(secondNumber)
-    PerformOperation(
-        firstNumber,
-        secondNumber)
-}
-// })
-let number = 0
-const changeSign = () => {
-    toggleNegative ? 
-    toggleNegative=false 
-    : toggleNegative=true
-    switch (number){
-        case number= initialNumber && toggleNegative===true:
-            initialNumber *-1
-        case number = operatingNumber &&toggleNegative===true:
-            operatingNumber * -1
+    
+    const enterNumber = (number) => {
+        let display = total+number
+        setTotal(display)
+        if (!operator){
+            let initialNumber = numbers['0']
+            return setNumbers({...numbers, '0':initialNumber+=number})
+            
+        }
+        let secondNumber = numbers['1']
+        return setNumbers({...numbers, '1':secondNumber+=number})
     }
-}
-PerformOperation(initialNumber, operator, operatingNumber)
-    switch(operator) {
-        case"+": ((initialNumber, operatingNumber) => (initialNumber + operatingNumber))
-    break;
-        case"-":  ((initialNumber, operatingNumber) => (initialNumber - operatingNumber))
-    break;
-        case"*": ((initialNumber, operatingNumber) => (initialNumber * operatingNumber))
-    break;
-        case"/": ((initialNumber, operatingNumber) => (initialNumber / operatingNumber))
-    break;
-        case"%": ((initialNumber, operatingNumber) => (initialNumber % operatingNumber))
-    default:
-        console.log('operating switch didn\'t run, what did you even do')
+ 
+    
+
+    
+    const convertNum = () => {
+        if (!operator){
+            let number = parseFloat(numbers['0'])
+            if (number > 0){
+                setNumbers({...numbers, '0':`-${number}`})
+                return setTotal(`-${number}`)
+            }
+            setNumbers({...numbers, '0':`${number*-1}`})
+            return setTotal(`${number*-1}`)
+        }
+        if (!numbers['1']){
+            return
+        }
+        let number = parseFloat(numbers['1'])
+        let splitTotal = total.split(operator)
+        let secondNumber = splitTotal[splitTotal.length-1]
+        if (number > 0){
+            secondNumber = `-${number}`
+            splitTotal[splitTotal.length-1] = secondNumber
+            console.log(splitTotal)
+            setNumbers({...numbers, '1':`-${number}`})
+            return setTotal(splitTotal.join(operator))
+        }
+        setNumbers({...numbers, '1':`${number*-1}`})
+        return setTotal(`${number*-1}`)
     }
-}
+
+    /**
+     * taking the second number that we want to convert
+     * we already have an operator so we want to split it there so there's an array
+     * we know the first number is the first, and the second is the last in the array
+     * grab the second number, assign it to a negative (see line 44)
+     * joining it back at the operator
+     * 
+     * now I need to do the opposit to return it as a positive
+     * this can be achieved with a helper function
+     */
+
+    const calculate = () => {
+        if (!numbers[0]||!numbers[1]||!operator){
+            console.log('error hits')
+            setError(...error, 'Oops, something went wrong. Hit AC or refresh the page and try again.')
+        } else{
+        let result = eval(`${numbers['0']} ${operator} ${numbers['1']}`)
+        setTotal(result)
+        }
+    }
 
 
-const clearIt = () => {
-    initialNumber = ''
-    operatingNumber = ''
-    operator = ''
-}
-//need to do string concatenation
-//and tehn parseint the string concatenation
-const calculate = () => {
+    const handleOperator = (symbol) => {
+       if (operator) {
+           return
+       }
+        let display = total+symbol
+        setTotal(display)
+        setOperator(symbol)
+    }
 
+    const clearIt = () => {
+        setNumbers({'0':'','1':''})
+        setOperator(null)
+        setTotal('')    
+    }
+
+
+console.log(numbers, operator)
     return (
 
         //I kind of didn't know what to do at first so I just went through and broke up each button
         <div className="container">
             <h1>React Calculator</h1>
+            <h4>{error}</h4>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{total}</div>
                 <div className="calc-row">
-                    <button 
-                    //      AC button
+                    <button
+                        onClick = {clearIt}
+                        //      AC button
                         className="calc-button calc-button-top"
-                        onClick={()=>clearIt()}
                     >AC
                     </button>
-                    <button 
-                    //      +/- button
+                    <button
+                        onClick ={convertNum}
+                        //      +/- button
                         className="calc-button calc-button-top"
-                        onClick={changeSign()}
                     >+/-
                     </button>
-                    <button 
-                    //      % button
+                    <button
+                        onClick={()=>handleOperator('%')}
+                        //      % button
                         className="calc-button calc-button-top"
-                        onClick={
-                            operate("%"),
-                            setIsOperating(true)
-                        }
                     >%
                     </button>
-                    <button 
-                    //      / button
-                    className="calc-button calc-button-op"
-                    onClick={
-                        operate("/"),
-                        setIsOperating(true)
-                    }
+                    <button
+                        onClick={()=>handleOperator('/')}
+                        //      / button
+                        className="calc-button calc-button-op"
                     >/
                     </button>
                 </div>
                 <div className="calc-row">
-                    <button 
-                    //      7 button
-                    className="calc-button"
-                    value='7'
-                    onClick={()=> numberPrinter()}
+                    <button
+                        onClick={()=>enterNumber(7)}
+                        //      7 button
+                        className="calc-button"
+                        value='7'
                     >7</button>
-                    <button 
-                    //      8 button
-                    className="calc-button"
-                    value='8'
-                    onClick={()=> numberPrinter()}
+                    <button
+                        onClick={()=>enterNumber(8)}
+                        //      8 button
+                        className="calc-button"
+                        value='8'
                     >8</button>
-                    <button 
-                    //      9 button
-                    className="calc-button"
-                    value='9'
-                    onClick={()=> numberPrinter()}
+                    <button
+                        onClick={()=>enterNumber(9)}
+                        //      9 button
+                        className="calc-button"
+                        value='9'
                     >9</button>
-                    <button 
-                    //      x button
-                    className="calc-button calc-button-op"
-                    onClick={
-                        operate("*"),
-                        setIsOperating(true)                        
-                    }
+                    <button
+                        onClick={()=>handleOperator('*')}
+                        //      x button
+                        className="calc-button calc-button-op"
                     >x</button>
                 </div>
                 <div className="calc-row">
-                    <button 
+                    <button
+                        onClick={()=>enterNumber(4)}
                         className="calc-button"
-                        value = "4"
-                        onClick={()=> numberPrinter()}    
+                        value="4"
                     >4</button>
-                    <button 
-                    className="calc-button"
-                    value="5"
-                    onClick={()=> numberPrinter()}
+                    <button
+                        onClick={()=>enterNumber(5)}
+                        className="calc-button"
+                        value="5"
                     >5</button>
-                    <button 
+                    <button
+                        onClick={()=>enterNumber(6)}
                         className="calc-button"
                         value="6"
-                        onClick={()=> numberPrinter()}
                     >6</button>
-                    <button 
+                    <button
+                        onClick={()=>handleOperator('-')}
                         className="calc-button calc-button-op"
-                        onClick={
-                            operate("-"),
-                            setIsOperating(true)                           
-                        }
                     >-</button>
                 </div>
                 <div className="calc-row">
-                    <button 
+                    <button
+                        onClick={()=>enterNumber(1)}
                         className="calc-button"
                         value="1"
-                        onClick={()=> numberPrinter()}
                     >1</button>
-                    <button 
+                    <button
+                        onClick={()=>enterNumber(2)}
                         className="calc-button"
                         value="2"
-                        onClick={()=> numberPrinter()}
                     >2</button>
-                    <button 
+                    <button
+                        onClick={()=>enterNumber(3)}
                         className="calc-button"
                         value="3"
-                        onClick={()=> numberPrinter()}
                     >3</button>
-                    <button 
+                    <button
+                        onClick={()=>handleOperator('+')}
                         className="calc-button calc-button-op"
-                        onClick={
-                            operate("+"),
-                            setIsOperating(true) 
-                        }
                     >+</button>
                 </div>
                 <div className="calc-row">
-                    <button 
+                    <button
+                        onClick={()=>enterNumber(0)}
                         className="calc-button width-2"
                         value="0"
-                        onClick={()=> numberPrinter()}
                     >0</button>
-                    <button 
+                    <button
+                        onClick={()=>enterNumber('.')}
                         className="calc-button"
-                        onClick={
-                            console.log(`button clicked`)
-                        }
                     >.</button>
-                    <button 
+                    <button
+                        // onClick={calculate}
+                        onClick={calculate}
                         className="calc-button calc-button-op"
-                        onClick={
-                            performOperation()
-                        }    
                     >=</button>
                 </div>
             </div>
         </div>
     )
 }
-
 export default Calculator
