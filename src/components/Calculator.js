87b4f227b08dfac941a74fe3294edb8aa67a4709
Mike Zahuta <mike.zahuta@gmail.com>
2020-12-28 15:11:52 -0500

    fixed multiple leading zeros and decimal point

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,80 +6,118 @@ const Calculator = (props) => {
   const [fixedInput, setFixedInput] = useState("");
   const [operator, setOperator] = useState("");
   const [currentNum, setCurrentNum] = useState("");
-  const [error, setErrorMessage] = useState('')
+  const [error, setErrorMessage] = useState("");
   // do I need an error state...? let's avoid if possible
-
 
   // Declare Functions //
   //////////////////////
   // ClearCalculator - done
   // HandleNumbers - done
   // HandleOperators - done
-  // CalcResult - v1 done, could need fine tuning
+  // CalcResult - done
+  // dont accept multiple leading 0s...
+  // Handle +/- button - done
+  // Handle % - extra bonus?
 
-  // Handle +/- button
-  // Handle % 
-  
-  // ErrorMessage (for hitting operator twice)
+  // ErrorMessage (for hitting operator twice) - done
+
   // Required Bonus options:
   // enable negative numbers with the - operator (or subtracting into negatives)
   // Implement the floating point . button in order to support decimals.
   // Don't allow more than one decimal per number!
+  // this also might make the 0 issue easier - currently accepting 0 as the first number multiple times
 
   const clearCalc = (e) => {
     calcResult(0);
     setFixedInput("");
     setCurrentNum("");
     setOperator("");
-    console.log('All Cleared')
+    setErrorMessage("");
+    console.log("All Cleared");
   };
 
   const handleNumbers = (e) => {
-      if (e.target.value !== '+' || e.target.value !== '-' || e.target.value !== '*' || e.target.value !== '/') {
-        let number = e.target.value
-        setCurrentNum(currentNum + number)
-      }
-      else{
-        setErrorMessage('number input error, clear and try again')
-      }
-  }
+    if (e.target.value === "0" && currentNum === "0") {
+    // setErrorMessage('Multiple zeros, clear');
+    setCurrentNum('0')
+    } else if (
+      e.target.value !== "+" ||
+      e.target.value !== "-" ||
+      e.target.value !== "*" ||
+      e.target.value !== "/"
+    ) {
+      let number = e.target.value;
+      setCurrentNum(currentNum + number);
+    } else {
+      setErrorMessage("number input error, clear and try again");
+    }
+  };
 
+  // operators are allowing multiple inputs and erasing state if a second is put in...
   const handleOperators = (e) => {
-    if (e.target.value === '+' || e.target.value === '-' || e.target.value === '*' || e.target.value === '/'){
-        let operator = e.target.value
-        console.log('CurrentNum at time of operator:', currentNum)
-        setFixedInput(currentNum)
-        setOperator(operator)
-        setCurrentNum('')
-        console.log('CurrentNum now:',currentNum,'and FixedInput now:', fixedInput)
+    if (operator) {
+      setErrorMessage("Error, clear & try again");
+    } else if (
+      e.target.value === "+" ||
+      e.target.value === "-" ||
+      e.target.value === "*" ||
+      e.target.value === "/"
+    ) {
+      let operator = e.target.value;
+      setFixedInput(currentNum);
+      setOperator(operator);
+      setCurrentNum("");
+    } else {
+      setErrorMessage("Operator error, clear and try again");
     }
-    else {
-        setErrorMessage('operator error, clear and try again')
-    }
-  }
-  
+  };
+
   const calculateResult = (e) => {
-      console.log('calcResult happening', e.target.value)
-      if (currentNum === "" || fixedInput === "") {
-        setErrorMessage("calculate input error, clear and try again");
-      } else if (operator === "+") {
-        let result = (Number(currentNum) + Number(fixedInput)).toString();
-        calcResult(result);
-      } else if (operator === "-") {
-        let result = (Number(currentNum) - Number(fixedInput)).toString();
-        calcResult(result);
-      } else if (operator === "*") {
-        let result = (Number(currentNum) * Number(fixedInput)).toString();
-        calcResult(result);
-      } else if (operator === "/") {
-        let result = (Number(currentNum) / Number(fixedInput)).toString();
-        calcResult(result);
-      }
-      else{
-          setErrorMessage('Calculating...')
-      }
+    console.log("calcResult happening", e.target.value);
+    if (currentNum === "" || fixedInput === "") {
+      setErrorMessage("calculate input error, clear and try again");
+    } else if (operator === "+") {
+      let result = (Number(currentNum) + Number(fixedInput)).toString();
+      calcResult(result);
+    } else if (operator === "-") {
+      let result = (Number(fixedInput) - Number(currentNum)).toString();
+      calcResult(result);
+    } else if (operator === "*") {
+      let result = (Number(currentNum) * Number(fixedInput)).toString();
+      calcResult(result);
+    } else if (operator === "/") {
+      let result = (Number(fixedInput) / Number(currentNum)).toString();
+      calcResult(result);
+    } else {
+      setErrorMessage("Calculating...");
+    }
+  };
+
+  // bonus requirement!
+  const handlePlusMinus = (e) => {
+    if (currentNum !== "-" + currentNum) {
+      setCurrentNum(Number(currentNum) * -1);
+    } else if (currentNum === "-" + currentNum) {
+      setCurrentNum(Number(currentNum) * -1);
+    } else {
+      console.log("neg/pos error, clear and try again");
+    }
+  };
+
+  // Handle % - done - just divides by 100
+  const handlePercent = (e) => {
+      setCurrentNum(Number(currentNum) / 100)
   }
 
+  //handle Decimal - thanks Andre : )
+  const handleDecimal = (e) => {
+      if (currentNum === '') {
+          setCurrentNum('0.')
+      }
+      else if (currentNum.indexOf('.') === -1) {
+          setCurrentNum(currentNum + '.')
+      }
+  }
 
   ////////////////////////////////////////////////
 
@@ -87,8 +125,12 @@ const Calculator = (props) => {
     <div className="container">
       <h1>React Calculator</h1>
       <div className="calc-container">
-        <p>{currentNum}{operator}{fixedInput}</p>
-        <div className="answer-box">{result}</div>
+        <p>
+          {fixedInput}
+          {operator}
+          {currentNum}
+        </p>
+        <div className="answer-box">{result || error}</div>
         <div className="calc-row">
           <button
             className="calc-button calc-button-top"
@@ -96,8 +138,14 @@ const Calculator = (props) => {
           >
             AC
           </button>
-          <button className="calc-button calc-button-top">+/-</button>
-          <button className="calc-button calc-button-top">%</button>
+          <button
+            className="calc-button calc-button-top"
+            value={"pm"}
+            onClick={(e) => handlePlusMinus(e)}
+          >
+            +/-
+          </button>
+          <button className="calc-button calc-button-top" value={'%'} onClick={(e)=> handlePercent(e)}>%</button>
           <button
             className="calc-button calc-button-op"
             value={"/"}
@@ -204,10 +252,16 @@ const Calculator = (props) => {
           >
             0
           </button>
-          <button className="calc-button" value={"."}>
+          <button className="calc-button" value={"."} onClick={(e)=> handleDecimal(e)}>
             .
           </button>
-          <button className="calc-button calc-button-op" value={'='} onClick={(e)=> calculateResult(e)}>=</button>
+          <button
+            className="calc-button calc-button-op"
+            value={"="}
+            onClick={(e) => calculateResult(e)}
+          >
+            =
+          </button>
         </div>
       </div>
     </div>
